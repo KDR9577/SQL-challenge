9d468ec9a600a32cef1783581f4d69f0aaebcdf0
KDRII <kennyr9577@gmail.com>
2024-01-13 20:28:00 -0600

    file additions1/13

--- EmployeeSQL/SQL-challenge.sql
+++ EmployeeSQL/SQL-challenge.sql
@@ -1,76 +1,45 @@
-CREATE TABLE "Departments" (
-    "dept_no" VARCHAR(255)   NOT NULL,
-    "dept_name" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
+--list the employee number, last name, first name, sex, and salary of each employee.
 
-CREATE TABLE "Dept_emp" (
-    "dept_no" VARCHAR(255)   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
-        "emp_no"
-     ),
-    CONSTRAINT "uc_Dept_emp_dept_no" UNIQUE (
-        "dept_no"
-    )
-);
+Select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+	from "Employees" as e
+	Left Join "Salaries" as s
+	on (e.emp_no=s.emp_no) 
+	order by e.emp_no; 
+	
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+--DATE FORMAT INCORRECT????
+Select first_name, last_name, hire_date
+	from "Employees"
+	where hire_date between '1-01-1986' and '12-31-1986';
+	
+--List the manager of each department along with their department number, department name, 
+--employee number, last name, and first name.
+--HOW TO JOIN 3 TABLES????
+Select d.dept_name, dm.dept_no, e.emp_no, e.last_name, e.first_name
+	from "Departments" as d
+	Left Join "Dept_manager" as dm 
+	on (d.dept_no=dm.dept_no)
+	from "Dept_manager" as dm 
+	Left Join "Employees" as e
+	on (dm.emp_no=e.emp_no);
+	
+--List the department number for each employee along with that employee’s employee number, 
+--last name, first name, and department name.
 
-CREATE TABLE "Dept_manager" (
-    "dept_no" VARCHAR(255)   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
-        "emp_no"
-     ),
-    CONSTRAINT "uc_Dept_manager_dept_no" UNIQUE (
-        "dept_no"
-    )
-);
+--List first name, last name, and sex of each employee whose first name is Hercules and whose 
+--last name begins with the letter B.
+--HOW TO SEARCH WITH LAST NAME BEGINS WITH B????
 
-CREATE TABLE "Employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR(255)   NOT NULL,
-    "birth_date" VARCHAR(255)   NOT NULL,
-    "first_name" VARCHAR(255)   NOT NULL,
-    "last_name" VARCHAR(255)   NOT NULL,
-    "sex" CHAR   NOT NULL,
-    "hire_date" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
-     ),
-    CONSTRAINT "uc_Employees_emp_title_id" UNIQUE (
-        "emp_title_id"
-    )
-);
+Select e.first_name, e.last_name, e.sex
+	from "Employees" as e
+	Where e.first_name = 'Hercules' and e.last_name ='B*';
+	
+--List each employee in the Sales department, including their employee number, 
+--last name, and first name.
 
-CREATE TABLE "Salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "emp_no"
-     )
-);
+--List each employee in the Sales and Development departments, including their employee number, 
+--last name, first name, and department name.
 
-CREATE TABLE "Titles" (
-    "title_id" VARCHAR(255)   NOT NULL,
-    "title" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "title_id"
-     )
-);
-
-ALTER TABLE "Departments" ADD CONSTRAINT "fk_Departments_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Dept_emp" ("dept_no");
-
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Dept_manager" ("dept_no");
-
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Salaries" ("emp_no");
-
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Dept_emp" ("emp_no");
-
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "Employees" ("emp_title_id");
+--List the frequency counts, in descending order, of all the employee last names 
+--(that is, how many employees share each last name).
+	
